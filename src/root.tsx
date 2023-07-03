44e4b9227d566a7064ee7fabaad487b9d7cb89f2
AJAL ODORA JONATHAN <43242517+ODORA0@users.noreply.github.com>
2023-07-03 13:01:43 +0300

    Add browser router to the root function

---
+++ src/root.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+export default function Root() {
+  return (
+    <BrowserRouter basename={window['getOpenmrsSpaBase']()}>
+      <Routes>
+        <Route path="/home" element={<Navigate to={'/dashboard/mother-child-health'} replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
