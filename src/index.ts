67157ebffa1d2e3479e69cde62dd37c4d42f4673
Hero <hherobiam@gmail.com>
2023-10-10 10:38:29 +0200

    (feat) registered a helper function to form engine, to auto generate infant ptracker id

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { getAsyncLifecycle, defineConfigSchema } from "@openmrs/esm-framework";
 import { configSchema } from "./config-schema";
+import { registerExpressionHelper } from "@openmrs/openmrs-form-engine-lib"
+import { generateInfantPTrackerId } from "./helpers/ptracker-form-helpers";
 
 require("./root.scss");
 
@@ -18,6 +20,7 @@ const options = {
 
 export function startupApp() {
   defineConfigSchema(moduleName, configSchema);
+  registerExpressionHelper("customGenerateInfantPTrackerId", generateInfantPTrackerId)
 }
 
 export const mnchClinicalDashboard = getAsyncLifecycle(
